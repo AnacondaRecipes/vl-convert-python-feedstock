de302ba021d64378b43e5e5e6b644d5f143da3c3
Andrii Vetroumov <avetroumov@anaconda.com>
2026-07-03 09:39:24 +0300

    Fix tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-if [ -f *.whl ]; then
-    # arm64 / aarch64: install pre-built wheel
-    cp vl_convert_python*/licenses/LICENSE .
-    ${PYTHON} -m pip install *.whl --no-deps --ignore-installed -vv
-else
-    # Source build (linux_64, osx_64)
-    export LIBSQLITE3_SYS_USE_PKG_CONFIG=1
-    export AWS_LC_SYS_CMAKE_BUILDER=1
+# Source build (linux_64, osx_64)
+export LIBSQLITE3_SYS_USE_PKG_CONFIG=1
+export AWS_LC_SYS_CMAKE_BUILDER=1
 
-    ${PYTHON} -m pip install . --no-deps --ignore-installed -vv
-fi
+${PYTHON} -m pip install . --no-deps --ignore-installed -vv
