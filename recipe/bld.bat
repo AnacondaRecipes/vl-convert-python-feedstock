77684a909ebb3db908d5e6079f9885ecd5229c4a
avetr-work <avetroumov@anaconda.com>
2026-07-06 13:04:05 +0300

    vl-convert-python 1.9.0.post1 (#4)
    * PKG-16090: vl-convert-python 2.0.0rc1
    
    * Fix tests

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,5 +3,5 @@
 set "LIBSQLITE3_SYS_USE_PKG_CONFIG=1"
 set "AWS_LC_SYS_CMAKE_BUILDER=1"
 
-%PYTHON% -m pip install . -vv
+%PYTHON% -m pip install --no-deps --ignore-installed . -vv
 if errorlevel 1 exit 1
